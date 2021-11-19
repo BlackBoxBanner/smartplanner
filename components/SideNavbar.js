b149b2ae41a36b0c0209f71d8c57446d928be8f1
kittitater <kittitater@gmail.com>
2021-11-20 02:58:27 +0700

    main

--- components/SideNavbar.js
+++ components/SideNavbar.js
@@ -5,7 +5,7 @@ export default function SideNavbar() {
     return (
       <nav className={styles.container}>
             <ul className={styles.Navul}>
-                <li><a href="#" className={styles.title}>Home</a></li>
+                <li><a href="#" className={styles.title}>Dashboard</a></li>
                 <li><a href="#" className={styles.title}>Calendar</a></li>
                 <li><a href="#" className={styles.title}>ToDoList</a></li>
                 <li><a href="#" className={styles.title}>Tracker</a></li>
