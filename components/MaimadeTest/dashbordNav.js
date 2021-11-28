f0c40a8210b476f51a7f15714a8554fa072fdaa2
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2021-11-29 02:36:27 +0700

    main

--- components/MaimadeTest/dashbordNav.js
+++ components/MaimadeTest/dashbordNav.js
@@ -1,14 +1,12 @@
-import styles from "@styles/Maimadetest/dashboard.module.css"
+import styles from "@styles/Maimadetest/dashboard.module.css";
 
 export default function DashbordNav() {
-  return (
-      <div className={styles.main}>
-          <div className={styles.header}>
-              this is the header of the dashboard
-          </div>
-          <div className={styles.sitetitle}>
-              Dashboard
-          </div>
-      </div>
-  )
+    return (
+        <div className={styles.main}>
+            <div className={styles.header}>
+                this is the header of the dashboard
+            </div>
+            <div className={styles.sitetitle}>Dashboard</div>
+        </div>
+    );
 }
--- pages/test.js
+++ pages/test.js
@@ -1,10 +1,12 @@
-import styles from "@styles/test.module.css"
-import DashbordNav from "@component/MaimadeTest/dashbordNav"
+import styles from "@styles/test.module.css";
+import DashbordNav from "@component/MaimadeTest/dashbordNav";
+import SideNav from "@component/MaimadeTest/sideNav";
 
 export default function test() {
     return (
         <div className={styles.main}>
-            <DashbordNav/>
+            <DashbordNav />
+            <SideNav />
         </div>
-    )
+    );
 }
