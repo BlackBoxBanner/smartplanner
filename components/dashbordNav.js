9d2f42d71e948f2a1ffcd1ec21d807525f7bf435
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2021-12-13 22:26:56 +0700

    set new Dashboard

--- components/dashbordNav.js
+++ components/dashbordNav.js
@@ -1,10 +1,113 @@
-import styles from "@styles/components/DashboardNav.module.css";
+import styles from "@styles/components/DashboradNav2.module.css";
+import Link from "next/link";
+import {
+  MdLogout,
+  MdAccountCircle,
+  MdSpaceDashboard,
+  MdCalendarToday,
+  MdList,
+  MdTrackChanges,
+} from "react-icons/md";
+import { GrClose, GrMenu } from "react-icons/gr";
+import useAuth from "@src/hook/auth";
 
-export default function DashbordNav() {
+export default function DashbordNav2() {
+  const { logout } = useAuth();
+
   return (
     <div className={styles.main}>
-      <div className={styles.header}>this is the header of the dashboard</div>
-      <div className={styles.sitetitle}>Dashboard</div>
+      <div className={styles.NavHead}>
+        <div className={styles.leftcontent}>Explanner</div>
+
+        {/* hanburger menu */}
+        <div className={styles.hamb}>
+          <button className={styles.hamburger}>
+            <div className={styles.menuIcon}>
+              <GrMenu size="1.5rem" />
+            </div>
+            <div className={styles.closeIcon}>
+              <GrClose size="1.5rem" />
+            </div>
+          </button>
+        </div>
+        {/* hanburger menu */}
+      </div>
+
+      <div className={styles.rightcontent}>
+        {/* <li>
+          <Link href="/">
+            <button>
+              <div className={styles.icon}>
+                //name of icon
+              </div>
+            </button>
+          </Link>
+        </li> */}
+
+        <li>
+          <Link href="/">
+            <button>
+              {" "}
+              {/* className={styles.btn} */}
+              <div className={styles.icon}>
+                <MdSpaceDashboard size="2rem" />
+                Dashboard
+              </div>
+            </button>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <button>
+              <div className={styles.icon}>
+                <MdCalendarToday size="2rem" />
+                Calendar
+              </div>
+            </button>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <button>
+              <div className={styles.icon}>
+                <MdList size="2rem" />
+                ToDoList
+              </div>
+            </button>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <button>
+              <div className={styles.icon}>
+                <MdTrackChanges size="2rem" />
+                Tracker
+              </div>
+            </button>
+          </Link>
+        </li>
+
+        <li>
+          <Link href="/">
+            <button>
+              <div className={styles.icon}>
+                <MdAccountCircle size="2rem" />
+                Profile
+              </div>
+            </button>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <button onClick={logout}>
+              <div className={styles.icon}>
+                <MdLogout size="2rem" />
+                SignOut
+              </div>
+            </button>
+          </Link>
+        </li>
+      </div>
     </div>
   );
 }
--- pages/user/dashboard.js
+++ pages/user/dashboard.js
@@ -1,21 +1,18 @@
 import Head from "next/head";
 import styles from "@styles/Dashboard.module.css";
-import DashbordNav from "@component/dashbordNav";
-import SideNav from "@component/sideNav";
 import { withProtected } from "@src/hook/route";
+import DashbordNav2 from "@component/dashbordNav";
 
 function Dashboard() {
-    return (
-        <div>
-            <Head>
-                <title>Dashboard</title>
-            </Head>
-            <SideNav/>
-            <main className={styles.main}>
-                <DashbordNav />
-            </main>
-        </div>
-    );
+  return (
+    <div>
+      <Head>
+        <title>Dashboard</title>
+      </Head>
+      <DashbordNav2 />
+      <main className={styles.main}></main>
+    </div>
+  );
 }
 
 export default withProtected(Dashboard);
