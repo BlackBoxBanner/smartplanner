7ce8cacea26f54e13f3ec94a2a48bdd1de4d7774
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2021-12-13 19:12:54 +0700

    Add new dashboard page.
    Creating new dashboard page but yet to be finish.

--- components/dashbordNav2.js
+++ components/dashbordNav2.js
@@ -1,15 +1,90 @@
 import styles from "@styles/components/DashboradNav2.module.css";
+import Link from "next/link";
+import {
+  MdLogout,
+  MdAccountCircle,
+  MdSpaceDashboard,
+  MdCalendarToday,
+  MdList,
+  MdTrackChanges,
+} from "react-icons/md";
 
 export default function DashbordNav2() {
   return (
     <div className={styles.main}>
       <div className={styles.leftcontent}>Explanner</div>
       <div className={styles.rightcontent}>
-        <ul className={styles.buttonList}>
-          {/* <li></li> */}
-          <li>Dashboard</li>
-          <li></li>
-        </ul>
+        
+        {/* <li>
+          <Link href="/">
+            <button>
+              <div className={styles.icon}>
+                //name of icon
+              </div>
+            </button>
+          </Link>
+        </li> */}
+
+        <li>
+          <Link href="/">
+            <button> {/* className={styles.btn} */}
+              <div className={styles.icon}>
+                <MdSpaceDashboard size="2rem" />
+                Dashboard
+              </div>
+            </button>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <button>
+              <div className={styles.icon}>
+                <MdCalendarToday size="2rem" />
+                Calendar
+              </div>
+            </button>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <button>
+              <div className={styles.icon}>
+                <MdList size="2rem" />
+                ToDoList
+              </div>
+            </button>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <button>
+              <div className={styles.icon}>
+                <MdTrackChanges size="2rem" />
+                Tracker
+              </div>
+            </button>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <button>
+              <div className={styles.icon}>
+                <MdAccountCircle size="2rem" />
+                Profile
+              </div>
+            </button>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <button>
+              <div className={styles.icon}>
+                <MdLogout size="2rem" />
+                SignOut
+              </div>
+            </button>
+          </Link>
+        </li>
       </div>
     </div>
   );
--- components/sideNav.js
+++ components/sideNav.js
@@ -1,47 +1,46 @@
 import styles from "@styles/components/sideNav.module.css";
 import {
-    MdLogout,
-    MdAccountCircle,
-    MdSpaceDashboard,
-    MdCalendarToday,
-    MdList,
-    MdTrackChanges,
+  MdLogout,
+  MdAccountCircle,
+  MdSpaceDashboard,
+  MdCalendarToday,
+  MdList,
+  MdTrackChanges,
 } from "react-icons/md";
-import useAuth from "@src/hook/auth"
+import useAuth from "@src/hook/auth";
+import Link from "next/link";
 
 export default function SideNav() {
-    const { logout } = useAuth();
-    return (
-        <div className={styles.main}>
-            <div className={styles.information}>
-                <button>
-                    <MdSpaceDashboard size="2rem" />
-                    Dashboard
-                </button>
-                <button>
-                    <MdCalendarToday size="2rem" />
-                    Calendar
-                </button>
-                <button>
-                    <MdList size="2rem" />
-                    ToDoList
-                </button>
-                <button>
-                    <MdTrackChanges size="2rem" />
-                    Tracker
-                </button>
-            </div>
+  const { logout } = useAuth();
+  return (
+    <div className={styles.main}>
+      <div className={styles.information}>
+        <button>
+          <MdSpaceDashboard size="2rem" />
+          Dashboard
+        </button>
+        <button>
+          <MdCalendarToday size="2rem" />
+          Calendar
+        </button>
+        <button>
+          <MdList size="2rem" />
+          ToDoList
+        </button>
+        <button>
+          <MdTrackChanges size="2rem" />
+          Tracker
+        </button>
+      </div>
 
-            <div className={styles.information}>
-                <button>
-                    <MdAccountCircle size="2rem" />
-                    Profile
-                </button>
-                <button onClick={logout}>
-                    <MdLogout size="2rem" />
-                    SignOut
-                </button>
-            </div>
-        </div>
-    );
+      <div className={styles.information}>
+        <MdAccountCircle size="2rem" />
+        Profile
+        <button onClick={logout}>
+          <MdLogout size="2rem" />
+          SignOut
+        </button>
+      </div>
+    </div>
+  );
 }
